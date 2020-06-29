74ffc5f9a6d502b1080adb99fe175278bd305570
Clarisse De Guzman <maryclarissedgzmn@gmail.com>
2020-06-28 20:17:00 -0400

    Initial Commit

---
+++ POS App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POS App
+//
+//  Created by Claire De Guzman on 2020-06-28.
+//  Copyright © 2020 Claire De Guzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
